be3d9181e286dcee90247586a4160e8a4391f610
Malcher <malcher.tm@gmail.com>
2019-06-02 14:35:13 -0300

    Add ScrollView e RaisedButton.

--- lib/main.dart
+++ lib/main.dart
@@ -29,29 +29,47 @@ class _HomeState extends State<Home>
         ],//Widget
       ),//appBar
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch, //Aline icon in center
-        children: <Widget>
-        [
-          Icon(Icons.person_pin, size:120.0, color: Colors.blue),
-          TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText:"Peso (KG)",
-              labelStyle: TextStyle(color: Colors.black)
-            ),//InputDecoration
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black, fontSize: 25.0),
-          ),//TextFild
-          TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Altura",
-              labelStyle: TextStyle(color: Colors.black)
-            ),//InputDecoration
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black,fontSize: 25.0),
-          ),//TextFild
-        ],//widget
-      ),//collum
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(8.5, 0.0, 8.5, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, //Aline icon in center
+          children: <Widget>
+          [
+            Icon(Icons.person_pin, size:120.0, color: Colors.blue),
+            TextField(keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText:"Peso (KG)",
+                  labelStyle: TextStyle(color: Colors.black)
+              ),//InputDecoration
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black, fontSize: 25.0),
+            ),//TextFild
+            TextField(keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura",
+                  labelStyle: TextStyle(color: Colors.black)
+              ),//InputDecoration
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black,fontSize: 25.0),
+            ),//TextFild
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                    onPressed: (){},
+                    child: Text("Calcular"),
+                    color: Colors.blue
+                ),//RaisedButton
+              ),//container,
+            ),//Padding
+            Text("Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey, fontSize: 25.0)
+            ),//text
+          ],//widget
+        ),//collum
+      ),//scrollview
     );//Scaffold
   }
 }
