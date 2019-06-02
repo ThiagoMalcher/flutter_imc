f27237861dd017427cec194faacf795999bd766d
Malcher <malcher.tm@gmail.com>
2019-06-02 12:14:41 -0300

    TextFild and Decoration.

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,36 @@ class _HomeState extends State<Home>
       appBar: AppBar(
         title: Text('Calculo do imc') ,
         centerTitle: true,
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.blueAccent,
+        actions: <Widget>
+        [
+          IconButton(icon: Icon(Icons.refresh), onPressed: (){})
+        ],//Widget
       ),//appBar
-    );//title
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, //Aline icon in center
+        children: <Widget>
+        [
+          Icon(Icons.person_pin, size:120.0, color: Colors.blue),
+          TextField(keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText:"Peso (KG)",
+              labelStyle: TextStyle(color: Colors.black)
+            ),//InputDecoration
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black, fontSize: 25.0),
+          ),//TextFild
+          TextField(keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura",
+              labelStyle: TextStyle(color: Colors.black)
+            ),//InputDecoration
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black,fontSize: 25.0),
+          ),//TextFild
+        ],//widget
+      ),//collum
+    );//Scaffold
   }
-
 }
